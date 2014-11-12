7f3c4bffe6112a7287764b4415be36c212352c47
ztj44 <zacharyjenkins@Zacharys-MacBook-Pro-2.local>
2014-11-12 17:20:32 -0600

    Initial Commit

---
+++ Server/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Server
+//
+//  Created by Zachary Jenkins on 11/12/14.
+//  Copyright (c) 2014 Zachary Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Server/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Server
+//
+//  Created by Zachary Jenkins on 11/12/14.
+//  Copyright (c) 2014 Zachary Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
